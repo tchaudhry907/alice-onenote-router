8165cf2db13516e4203b47fe7aeb991bcfd26aa8
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-08-15 10:31:59 -0400

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
+// pages/api/hello.js
 export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello from API' })
+  res.status(200).json({ ok: true, msg: "Hello from API" });
 }
