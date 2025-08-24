b16bb83e181612861c1fc4b1fc66d9eaa4657073
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-08-23 22:02:53 -0400

    Refactor debug-cookies API response format

--- pages/api/debug/debug-cookies.js
+++ pages/api/debug/debug-cookies.js
@@ -1,9 +1,5 @@
-
-// pages/api/debug-cookies.js
 export default function handler(req, res) {
-  res.status(200).json({
-    host: req.headers.host,
-    app_base_url: process.env.APP_BASE_URL,
-    cookies: req.headers.cookie || "(none)"
-  });
+  const cookies = req.headers.cookie || '';
+  res.setHeader('Content-Type','application/json');
+  res.status(200).send(JSON.stringify({ cookies }, null, 2));
 }
