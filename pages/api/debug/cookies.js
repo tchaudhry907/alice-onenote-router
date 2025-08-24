6a2a1f11d357da76966eb08328d23a35ca2221f4
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-08-23 22:40:59 -0400

    Refactor cookies API handler for JSON response

--- pages/api/debug/cookies.js
+++ pages/api/debug/cookies.js
@@ -1,12 +1,5 @@
-// pages/api/debug-cookies.js
-export default function handler(req, res) {
-  res.status(200).json({
-    cookies: req.cookies ?? {},
-    headers: {
-      cookie: req.headers?.cookie ?? null,
-      host: req.headers?.host ?? null,
-      "user-agent": req.headers?.["user-agent"] ?? null,
-      referer: req.headers?.referer ?? null
-    }
-  });
+// pages/api/debug/cookies.js
+export default async function handler(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.status(200).send(JSON.stringify({ cookies: req.cookies ?? {} }, null, 2));
 }
