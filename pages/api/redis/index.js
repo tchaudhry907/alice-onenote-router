c547944d58d2ba381bb52d733e5d092db1de12f2
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-02 19:25:51 -0400

    Update Redis API root response with routes

--- pages/api/redis/index.js
+++ pages/api/redis/index.js
@@ -1,3 +1,11 @@
 export default async function handler(req, res) {
-  return res.status(200).json({ ok: true, message: 'Redis API root' });
+  res.status(200).json({
+    ok: true,
+    routes: [
+      "/api/redis/ping",
+      "/api/redis/get?key=someKey",
+      "/api/redis/set?key=someKey&value=someValue",
+      "/api/redis/set?key=expiringKey&value=hello&ttl=60"
+    ]
+  });
 }
