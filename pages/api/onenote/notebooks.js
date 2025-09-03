14260ef7b2fd39357c2af8bf6e3b24b9edf32a2b
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-03 18:13:31 -0400

    Add API handler for OneNote notebooks

---
+++ pages/api/onenote/notebooks.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+  const r = await fetch(`${process.env.APP_BASE_URL}/api/onenote/proxy?url=https://graph.microsoft.com/v1.0/me/onenote/notebooks`);
+  const text = await r.text();
+  res.status(r.status).send(text);
+}
