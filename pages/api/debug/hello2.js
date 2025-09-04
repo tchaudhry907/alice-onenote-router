1dfc4a261387ab8396b2c19ac0c8aacf68ba648a
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-04 18:18:34 -0400

    Update hello2.js

--- pages/api/debug/hello2.js
+++ pages/api/debug/hello2.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({
     text: "hello2",
     UPSTREAM_REDIS_REST_URL: process.env.UPSTREAM_REDIS_REST_URL || "unknown",
-    VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || "unknown"
+    VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || "unknown",
+    timestamp: new Date().toISOString()
   })
 }
