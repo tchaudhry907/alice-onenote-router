457437d157f7e99059b8f18dfbe75a2231d94d3a
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-04 11:50:48 -0400

    Refactor Redis API handler and update routes

--- pages/api/redis/index.js
+++ pages/api/redis/index.js
@@ -1,11 +1,15 @@
-export default async function handler(req, res) {
+// /pages/api/redis/index.js
+export default function handler(req, res) {
   res.status(200).json({
     ok: true,
     routes: [
-      "/api/redis/ping",
-      "/api/redis/get?key=someKey",
-      "/api/redis/set?key=someKey&value=someValue",
-      "/api/redis/set?key=expiringKey&value=hello&ttl=60"
-    ]
+      '/api/redis/ping',
+      '/api/redis/get?key=foo',
+      '/api/redis/set?key=foo&value=bar&ex=60',
+      '/api/redis/ttl?key=foo',
+      '/api/redis/del?key=foo',
+      '/api/redis/expire?key=foo&seconds=120',
+      '/api/redis/debug'
+    ],
   });
 }
