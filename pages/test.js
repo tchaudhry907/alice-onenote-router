c669da339b24a464ab325cad4ce501c60f04e1c0
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-06 15:07:03 -0400

    Simplify redirect logic in test.js

--- pages/test.js
+++ pages/test.js
@@ -1,15 +1,5 @@
 // pages/test.js
-// Always redirect /test -> /onenote-test
-
 export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: "/onenote-test",
-      permanent: false,
-    },
-  };
+  return { redirect: { destination: "/onenote-test", permanent: false } };
 }
-
-export default function TestRedirect() {
-  return null;
-}
+export default function TestRedirect() { return null; }
