26ea99dc14c76842e4041ada281803df45b4eb86
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-16 19:47:26 -0400

    Delete pages/api/cron/index-recent.js

--- pages/api/cron/index-recent.js
+++
@@ -1,7 +0,0 @@
-// pages/api/cron/index-recent.js
-export default async function handler(_req, res) {
-  return res.status(200).json({
-    ok: true,
-    message: "Cron disabled in v2 during setup. Use Diagnostics buttons instead.",
-  });
-}
