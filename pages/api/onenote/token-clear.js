35895d55a5d45d9ba8aa657917b7e2c6c1f711d0
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-17 18:17:55 -0400

    Add additional tokens to clear in token-clear.js

--- pages/api/onenote/token-clear.js
+++ pages/api/onenote/token-clear.js
@@ -2,7 +2,7 @@
 import { kvDel } from '@/lib/kv';
 
 export default async function handler(req, res) {
-  const keys = ['ms:access_token','graph:access_token','access_token'];
+  const keys = ['graph:access_token','ms:access_token','access_token','ms:refresh_token','auth:access_token'];
   const results = {};
   for (const k of keys) results[k] = await kvDel(k);
   res.status(200).json({ ok: true, deleted: results });
