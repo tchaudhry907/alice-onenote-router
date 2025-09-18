973354289e2b35a1522e2259c8afdb68754c4756
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-18 17:20:12 -0400

    Remove __dirname polyfill from next.config.mjs
    Removed polyfill for __dirname in ESM.

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-/** polyfill __dirname in ESM */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
