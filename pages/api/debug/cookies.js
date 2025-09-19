c377a87ff68eab50ed2184b909b0645d6e52c21e
tchaudhry907 <156623357+tchaudhry907@users.noreply.github.com>
2025-09-19 11:46:36 -0400

    Fix comment syntax in cookies.js

--- pages/api/debug/cookies.js
+++ pages/api/debug/cookies.js
@@ -1,4 +1,4 @@
-// pages/api/debug/cookies.js
+// pages/api/debug/cookies.js 
 export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.status(200).send(JSON.stringify({ cookies: req.cookies ?? {} }, null, 2));
